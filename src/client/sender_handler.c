70be62169545bed98646465fc0a7b5aa9a714cb3
brycepanza <bep236@nau.edu>
2025-11-09 20:34:56 -0700

    Updated client sending logic. Patched up other parts to match submission requirements

--- src/client/sender_handler.c
+++ src/client/sender_handler.c
@@ -2,6 +2,7 @@
 #include "sender_handler.h"
 #include "main.h"
 #include "../chat_node.h"
+#include "../message.h"
 
 // function definitions
 
@@ -12,50 +13,139 @@
   different commands or a note the user put in.
 */
 
-void *sender_handler(void* args)
+// updates state if not already joined and sends message to server
+static void handle_join(int client_socket, ThreadArgs *args)
 {
 
-  ThreadArgs *local_args = (ThreadArgs *)args;
+}
 
-  NodeState state= local_args->state;
+// updates state if not already left and sends message to server
+static void handle_leave(int client_socket, ThreadArgs *args)
+{
 
-  char user_input[MESSAGE_LEN];
+}
 
-  while(state != EXIT){
-    scanf("%s", user_input);
-    if(strncmp(user_input, "JOIN", 4) == 0)
-    {
-      if(state != JOINED)
-      {
-        printf("Already joined\n");
-      }
-      else 
-      {
-        state = JOINED;
-      }
-    }
-    else if(strcmp(user_input, "LEAVE") == 0 && state != JOINED)
-    {
-      // disconnect user from the server function
-      state = SUSPENDED;
+// send a message to the server
+static void handle_post(int client_socket, Message message)
+{
 
-      // send leave message to server function
-    }
-    else if(strcmp(user_input, "SHUTDOWN") == 0)
-    {
-      // send shutdown message to server function
-      state = EXIT;
-    }
-    else if(strcmp(user_input, "SHUTDOWN ALL") == 0)
-    {
-      // send shutdown all message to server function
-      state = EXIT;
-    }
-    else
-    {
-      // send regular message to server function
-    }
-  }
+}
+
+// updates state and sends a message to the server
+static void handle_shutdown(int client_socket, ThreadArgs *args)
+{
+
+}
+
+void *sender_handler(void* args)
+{
+	// interpret arguments structure
+	ThreadArgs *local_args = (ThreadArgs *)args;
+
+	Message message;
+
+	// buffer for reading user input
+	// ############################ TODO: CHANGE SIZE TO ACCOUNT FOR COMMAND ??? ##########################
+	char user_input[MESSAGE_LEN];
+
+	// descriptor for connections made to the server
+	int client_socket;
+	struct sockaddr_in client_address;
+
+	// create addr struct
+	client_address.sin_family = AF_INET;
+	client_address.sin_addr.s_addr = inet_addr(property_get_property(local_args->property_list, "SERVER_IP"));
+	client_address.sin_port = htons(atoi(property_get_property(local_args->property_list, "SERVER_PORT")));
+
+	// ignore SIGPIPE on connection closed
+	signal(SIGPIPE, SIG_IGN);
+
+	// loop while program is running
+	while (local_args->state != EXIT)
+	{
+		// get user input
+			// ################## READS INPUT PROPERLY ??? ##################
+		scanf("%s", user_input);
+		// interpret as proper message struct
+		interpret_message(user_input, &message);
+
+		client_socket = socket(AF_INET, SOCK_STREAM, 0);
+		if (client_socket == -1)
+		{
+			perror("Client socket creation error");
+			continue;
+		}
+
+		// establish connection with server
+		if (connect(client_socket, (struct sockaddr *)&client_address, sizeof(client_address)) == -1)
+		{
+			perror("Connection error");
+			close(client_socket);
+			continue;
+		}
+
+		// determine proper action
+		switch (message.type)
+		{
+			case JOIN:
+				handle_join(client_socket, local_args);
+				break;
+			case LEAVE:
+				handle_leave(client_socket, local_args);
+				break;
+			case POST:
+				handle_post(client_socket, message);
+				break;
+			case SHUTDOWN:
+			case SHUTDOWN_ALL:
+				handle_shutdown(client_socket, local_args);
+				break;
+		}
+
+		// close connection
+		close(client_socket);
+	}
+
+	// NodeState state = local_args->state;
+
+	// char user_input[MESSAGE_LEN];
+
+	// while(state != EXIT)
+	// {
+	// 	scanf("%s", user_input);
+	// 	if(strncmp(user_input, "JOIN", 4) == 0)
+	// 	{
+	// 		if(state != JOINED)
+	// 		{
+	// 			printf("Already joined\n");
+	// 		}
+	// 		else 
+	// 		{
+	// 			state = JOINED;
+	// 		}
+	// 	}
+	// 	else if(strcmp(user_input, "LEAVE") == 0 && state != JOINED)
+	// 	{
+	// 		// disconnect user from the server function
+	// 		state = SUSPENDED;
+
+	// 		// send leave message to server function
+	// 	}
+	// 	else if(strcmp(user_input, "SHUTDOWN") == 0)
+	// 	{
+	// 		// send shutdown message to server function
+	// 		state = EXIT;
+	// 	}
+	// 	else if(strcmp(user_input, "SHUTDOWN ALL") == 0)
+	// 	{
+	// 		// send shutdown all message to server function
+	// 		state = EXIT;
+	// 	}
+	// 	else
+	// 	{
+	// 		// send regular message to server function
+	// 	}
+	// }
   
 
   return NULL;
--- src/client/sender_handler.h
+++ src/client/sender_handler.h
@@ -2,6 +2,22 @@
 #ifndef SENDER_HANDLER_H
 #define SENDER_HANDLER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// socket/bind/listen/accept
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// read/write/close
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <signal.h>
+
 // function prototypes
 void *sender_handler(void *args);
 
